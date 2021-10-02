b493f2e28c81c94d7927492f6d1842306453b995
1839581 <89651374+1839581@users.noreply.github.com>
2021-10-02 01:53:39 -0400

    Add files via upload

---
+++ lab5.php
@@ -0,0 +1,4 @@
+
+welcome <?php echo $_GET["name"]; ?><br>
+Color <?php echo $_GET["color"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
--- lab5step2.php
+++ lab5step2.php
@@ -1,1 +1,2 @@
+<P><?= var_dump( $_SERVER['HTTP_USER_AGENT']) ?></p>
 <p><?= var_dump($_SERVER) ?></p>
