e439fcfb01314f1ccc42aa4a45d9103265d1d547
1839581 <89651374+1839581@users.noreply.github.com>
2021-11-23 18:39:14 -0500

    Add files via upload

--- led.php
+++ led.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Raspberry Pi WiFi Controlled LED</title>
+</head>
 <body>
+    <h1>Control LED using Raspberry Pi Webserver</h1>      
+    <form method="get" action="index.php">                
+        <input type="submit" style = "font-size: 14 pt" value="OFF" name="off">
+        <input type="submit" style = "font-size: 14 pt" value="ON" name="on">
+    </form>​​​
+                         
     <?php
-        $state=1;
-        if($state=1) {
-            shell_exec("gpio write 7 1");
-            echo "The light is On";
-        }
+        shell_exec("/usr/local/bin/gpio -g mode 27 out");
+        if(isset($_GET['off'])){
+            {
+                echo "LED is off";
+                shell_exec("/usr/local/bin/gpio -g write 27 0");
+            }
                 
-        else
-        {
-            shell_exec("gpio write 7 0");
-            echo "The light is Off";
+            else if(isset($_GET['on']))
+            {
+                echo "LED is on";
+                shell_exec("/usr/local/bin/gpio -g write 27 1");
+            }
         }
     ?>
-
 </body>
-
 </html>
