28f2bfa0316a973e1508dfd214cfadb09f9665c7
1839581 <89651374+1839581@users.noreply.github.com>
2021-11-23 22:44:11 -0500

    Add files via upload

--- bme280.php
+++ bme280.php
@@ -3,23 +3,27 @@
         <?php
             
             
-            $raw = /var/www/html/WebServersClass/raspberry-pi-bme280/`./bme280`;
+            $raw = `./bme280`;
             $deserialized = json_decode($raw);
-            echo "The sensor is the:"
+            echo "The sensor is the: ";
             echo $deserialized->sensor;
-            echo ""
-            echo "The temperature is:"
+            echo "<br>";
+            echo "<br>";
+            echo "The temperature is: ";
             echo $deserialized->temperature;
-            echo ""
-            echo "The altitude is:"
+            echo "<br>";
+            echo "<br>";
+            echo "The altitude is: ";
             echo $deserialized->altitude;
-            echo ""
-            echo "The pressure is:"
+            echo "<br>";
+            echo "<br>";
+            echo "The pressure is: ";
             echo $deserialized->pressure;
-            echo ""
-            echo "The humidity is:"
+            echo "<br>";
+            echo "<br>";
+            echo "The humidity is: ";
             echo $deserialized->humidity;
-            echo ""
+            echo "<br>";
             
             
             
