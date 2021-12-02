c6a0c5fa6b65b5f8f30795c18e701b041911d633
1839581 <89651374+1839581@users.noreply.github.com>
2021-12-02 18:04:33 -0500

    Add files via upload

---
+++ elec-resp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+Name: <?php echo $_GET["username"]; ?><br>
+
+Favorite components: <?php echo $_GET["comp_list"] ?><br>
+
+Favorite Piece of Machinery: <?php echo $_GET["mac_list"] ?><br>
+
+Resistor Color: <?php echo $_GET["res"] ?><br>
+
+</body>
+</html>
